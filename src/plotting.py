536259d12aaf8c9fc66f23d4893036081cff7013
ognjen <milosevicognjen03@gmail.com>
2026-04-05 14:33:41 +0200

    fix warnings for graphs

--- src/plotting.py
+++ src/plotting.py
@@ -83,10 +83,13 @@ def plot_advanced_features(df):
         sns.violinplot(
             x="Rooms_Cat",
             y="Total_Price_EUR",
+            hue="Rooms_Cat",
             data=df_viz,
             ax=axes[0],
             order=["0.5", "1.0", "1.5", "2.0", "2.5", "3.0", "3.5", "4.0", "4.5", "5+"],
             palette="muted",
+            legend=False,
+            dodge=False,
         )
         axes[0].set_title("Price Variance by Room Count")
 
